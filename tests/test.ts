09e9d9994bbecaabf2a8bc151476058e1709f711
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:45:53 -0500

    Refactor test.ts to use getByRole instead of locator

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,12 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
 	await page.goto("/");
-	const h1 =
-		"Welcome to the LJ Dev -- SvelteKit Typescript Tailwind Pug Starter.";
-	await expect(page.locator("h1")).toHaveText(h1);
+	await expect(
+		page.getByRole("heading", {
+			name: "Hit the ground running with Sveltekit, Typescript, Tailwind & Pug.",
+		}),
+	).toBeVisible();
 });
+
+//* This Highlighted
+// vs. this not highlighted
