39feb47cf5feb7b754ad580b6963908a5298b941
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:44:27 -0500

    Add Logos.svelte component

---
+++ src/lib/components/molecules/Logos.svelte
@@ -0,0 +1,37 @@
+<!--
+@component
+Here's some documentation for this component.
+-->
+
+<script lang="ts">
+	// context api
+	import { getContext } from "svelte";
+
+	// components
+	import LogoBlock from "$molecules/LogoBlock.svelte";
+
+	const brands = getContext("brands") ?? [];
+</script>
+
+<template lang="pug">
+	header(
+		class=`
+			mb-[2ch]
+			px-8
+			gap-x-8
+			grid
+			grid-cols-2
+			sm:gap-x-0
+			sm:gap-y-[1ch]
+			sm:grid-cols-7
+			sm:px-0`)
+		|
+		+each('brands as brand, index')
+			//- logo
+			LogoBlock(
+				brand!="{ brand }")
+
+			//- plus sign
+			svelte:element(
+				class="hidden place-content-center sm:grid",
+				this!="{ index != brands.length - 1 ? 'div' : null }") +</template>
