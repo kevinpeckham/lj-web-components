7756aeb513da11e05075269999cab24857d75ca4
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:43:46 -0500

    Refactor ButtonLink component props and styles

--- src/lib/components/atoms/ButtonLink.svelte
+++ src/lib/components/atoms/ButtonLink.svelte
@@ -1,21 +1,50 @@
 <!--
 @component
 An <a> styled to look like a button.
-* prop url: string  -- link target url
-* prop title: string -- link title
-* slot (default) -- link text
+* prop classes: string; (optional) -- additional classes;
+* prop cta: Link;
+* cta.href: string;
+* cta.label: string;
+* cta.rel: string;
+* cta.target: string;
+* slot (optional) -- link text
 -->
 <script lang="ts">
-	export let targetUrl = "#";
-	export let title = "";
+	// types
+	import type { Link } from "$types/types";
+
+	// props
+	export let classes = "";
+	export let cta: Link;
+	$: href = cta.href ?? null;
+	$: label = cta.label ?? "";
+	$: rel = cta.rel ?? null;
+	$: target = cta.target ?? null;
+	$: title = cta.title ?? "";
 </script>
 
 <template lang="pug">
-	.mb-16.w-full(class="sm:mb-24")
-		a.text-15.inline-block.rounded.bg-transparent.px-4.py-2.outline.outline-1.outline-neutral-50.transition-all(
-			class="hover:bg-accent hover:text-primary hover:outline-transparent",
-			href!="{ targetUrl }",
-			title!="{ title }"
-		)
-			slot
-</template>
+	a(
+		class!=`
+			bg-transparent
+			block
+			hover:bg-accent
+			hover:outline-transparent
+			hover:text-primary
+			max-w-fit
+			outline-current
+			outline
+			px-5
+			py-[.75ch]
+			rounded
+			text-[.85em]
+			transition-all
+			{ classes }`,
+		href!="{ href }",
+		rel!="{ rel }",
+		target!="{ target }",
+		title!="{ title }") { label }
+		//- slot
+		slot
+
+	|</template>
