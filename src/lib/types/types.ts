16ce7c2116aea8a5e26ce4ce1df80828573a4a34
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:45:01 -0500

    Update types.ts with new interfaces and type

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,1 +1,18 @@
-// a great place to put some custom types
+// a great place to put types that are used in multiple places
+export interface Link {
+	[key: string]: unknown;
+	label: string;
+	href: string;
+	rel?: string;
+	target?: string;
+	title?: string;
+}
+export interface Image {
+	[key: string]: unknown;
+	src: string;
+	alt?: string;
+	loading?: string;
+	height?: string;
+	width?: string;
+}
+export type Brand = Link;
