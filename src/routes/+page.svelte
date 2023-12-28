68e5f2a512e7218e29b0c370b41be883b1992a2d
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:45:41 -0500

    Refactor page component and add context API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,73 @@
-<!-- Example Svelte Page / Starter Web Page-->
 <script lang="ts">
+	// set context api
+	import { setContext } from "svelte";
+
 	// components
 	import ButtonLink from "$atoms/ButtonLink.svelte";
-	import Footer from "$components/organisms/Footer.svelte";
-	import LogosGrid from "$molecules/LogosGrid.svelte";
-	import PreFooter from "$atoms/PreFooter.svelte";
+	import Logos from "$molecules/Logos.svelte";
 
 	// types
-	import type { LayoutData } from "./$types";
+	import type { PageData } from "./$types";
 
 	// catch data from layout function in +layout.ts
-	export let data: LayoutData;
+	export let data: PageData;
+
+	// pass brands content to Logos component via context api
+	$: setContext("brands", data.brands);
 </script>
 
 <template lang="pug">
-	//- head
-	svelte:head
-		title Svelte Pug Tailwind Typescript | Starter Template
-		meta(
-			content="A node starter template for the Svelte, Typescript, TailwindCSS, and Pug stack.",
-			name="description"
-		)
+	//- outer container
+	main(
+		class=`
+			gap-y-2
+			grid
+			grid-cols-1
+			min-h-screen
+			place-content-start
+			justify-items-center
+			text-center
+			sm:text-left
+			sm:pt-[4.5em]
+			md:pt-24`)
+		|
 
-	//- body
-	main.relative.grid.grid-cols-1.min-h-screen.place-items-center.p-4.pb-48
-		div(class="sm:max-w-lg lg:max-w-xl xl:max-w-2xl")
-			.mb-20
-				LogosGrid(brands!="{ data.brands }")
+		//- inner container
+		div(
+			class=`
+				p-[18px_36px_216px]
+				sm:p-0
+				sm:max-w-sm
+				md:max-w-md
+				`)
+			|
 
-			.text-center(class="sm:text-left")
-				//- headline
-				h1.text-22.mb-3.font-semibold.text-accent
-					| Hit the ground running with
-					br
-					| Sveltekit, Typescript, Tailwind &amp; Pug.
+			//- logos
+			Logos
 
-				//- text
-				p.text-md.mb-10.text-neutral-100.text-18(class="sm:mb-8 leading-relaxed")
-					| This project is an open source starter template for Node projects using SvelteKit, Typescript, TailwindCSS, and Pug. Designed to help get you up and running quickly with the SkinnyPug Stack.
+			//- headline
+			h1 { data.body.headline }
 
-				//- cta
-				ButtonLink(
-					targetUrl="https://github.com/lightning-jar/lj-sveltekit-ts-tailwind-pug-starter",
-					title="View project on Github"
-				)
-					| Github
+			//- text
+			p(
+				class="leading-relaxed opacity-90 mb-8")
+				//- use html to render rich text
+				+html('data.body.text')
 
-	//- pre-footer
-	PreFooter(
-		targetUrl="https://pugify.dev",
-		title="convert HTML to pug"
-	)
-		svelte:fragment(slot="default") Also check out the ad-free HTML to Pug converter @ https://pugify.dev
+			//- button link
+			ButtonLink(
+				cta!="{ data.body.cta }")
 
-	//- footer
-	Footer(
-		authorLink="https://fosstodon.org/@kevinpeckham",
-		orgLink="https://lightningjar.com"
-	)
-		svelte:fragment(slot="author") Kevin Peckham
-		svelte:fragment(slot="org") Lightning Jar
-</template>
+	|</template>
+
+<style lang="postcss">
+	/* examples of how Tailwind may be used within the style section of a Svelte component */
+
+	h1 {
+		@apply font-semibold
+			leading-snug
+			mb-[.75ch]
+			text-[1.425em]
+			text-accent;
+	}
+</style>
