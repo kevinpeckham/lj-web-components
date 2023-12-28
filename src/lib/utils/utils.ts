f1d6d52a497077776be9a0cc20f88def691bfc11
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-12-28 11:45:07 -0500

    Add slugify utility function

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,1 +1,8 @@
 // a great place to put some utility functions
+
+// slugify -- a very simple slugify function
+// takes a string and returns a slugified version
+// e.g. "Hello World" => "hello-world"
+export function slugify(string: string) {
+	return string.replace(/\W/g, "-").replace(/--*/gi, "-").toLowerCase();
+}
