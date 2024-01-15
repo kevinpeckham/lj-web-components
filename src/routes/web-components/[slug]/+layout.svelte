ee595f3970ce70d19ab9506a1f1c914c96cf116b
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-15 15:10:33 -0500

    Add web component layout file

---
+++ src/routes/web-components/[slug]/+layout.svelte
@@ -0,0 +1,25 @@
+<!--
+@component
+Here's some documentation for this component.
+-->
+
+<script lang="ts">
+	// import child page data
+	import { page } from "$app/stores";
+</script>
+
+<template lang="pug">
+	//- head metadata
+	svelte:head
+		title { $page.data.metaTitle ?? "TITLE" }
+		+if('$page.data.metaDescription')
+			meta(
+				content!="{ $page.data.metaDescription }",
+				name="description")
+		//- web component script
+		+if('$page.data.webComponentScript')
+			script(
+				async="true",
+				src!="{ $page.data.webComponentScript }")
+	slot
+	|</template>
