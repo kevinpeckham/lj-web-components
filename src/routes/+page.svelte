70f5bd52f4904ad96d8efdb8f138d30ce1da82fd
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-15 15:10:22 -0500

    Remove scrolling-stat component from +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,4 @@
 
 <div class="w-screen h-[300vh] flex justify-center items-center">
 	<div>Hello World.</div>
-	<!-- <scrolling-stat
-		number="97.2"
-		unit="k"
-		text="splunges per widget lorem ipsum dolor sit">
-	</scrolling-stat> -->
 </div>
