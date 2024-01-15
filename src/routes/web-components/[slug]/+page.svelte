b2c6736a027ecce4b4b0c386b148b934d37bc986
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-15 15:10:42 -0500

    Add new web component page

---
+++ src/routes/web-components/[slug]/+page.svelte
@@ -0,0 +1,22 @@
+<!--
+@component
+Here's some documentation for this component.
+-->
+
+<script lang="ts">
+	// types
+	import type { PageData } from "./$types";
+
+	// data
+	export let data: PageData;
+	$: console.log(data);
+</script>
+
+<template lang="pug">
+	main.page-x-padding.main-y-padding
+		svelte:element(
+			text="splunges per widget",
+			this!="{ data.slug }",
+			unit="k")
+
+	|</template>
