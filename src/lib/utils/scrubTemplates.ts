862f9141d93e0b9135feccb9f3fe4ebcb59987df
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-27 16:37:15 -0500

    Add scrubTemplates utility function

---
+++ src/lib/utils/scrubTemplates.ts
@@ -0,0 +1,24 @@
+export function scrubTemplates(str: string): string {
+	// match style tags
+	const templateMatch = str.match(
+		/(?:<(?:span|div){1}[^<>\n]*?(?:id="container")>){1}(?:.|\n)*<\/(?:span|div){1}>/gi,
+	);
+
+	// iterate through style tags and replace with scrubbed text
+	if (templateMatch) {
+		for (const match of templateMatch) {
+			const styleScrubbed = match
+				.replaceAll(new RegExp(String.raw`\\t+?`, "g"), "")
+				.replaceAll(/(?:\n|\\n)+?/g, "") // remove newlines
+				.replaceAll(/(?:\t|\\t)+?/g, " ") // remove tabs
+				.replaceAll(/(?:\s){2,}/g, " "); // remove double+ spaces
+			// .replaceAll(/(?:([;:{}>])\s)+?/g, "$1") // remove single space after [;:{}>]
+			// .replaceAll(/(?:;})/g, "}"); // remove single semicolon before closing bracket
+			str = str.replace(match, styleScrubbed);
+		}
+	}
+
+	return str;
+}
+
+export default scrubTemplates;
