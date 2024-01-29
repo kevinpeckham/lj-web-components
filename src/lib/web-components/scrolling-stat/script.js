2e9961e4f511649a18bda024456073a7b4ea9327
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-28 21:00:28 -0500

    Update function to handle edge cases
    This commit updates the function to handle edge cases, improving the overall reliability and robustness of the code.

--- src/lib/web-components/scrolling-stat/script.js
+++ src/lib/web-components/scrolling-stat/script.js
@@ -1,99 +1,62 @@
 /** @copyright 2024 Lightning Jar - "Scrolling Stat" web component - License MIT */
-/** @author Kevin Peckham */
+`/** @author Kevin Peckham */`;
 /** @license MIT */
 /** @version 0.0.1 */
-/** {@link https://www.lj-cdn.dev/web-components/scrolling-stat} */
+/** {@link https://lj-cdn.dev/web-components/scrolling-stat} */
 
 /**
  * Scrolling Stat Web Component
  * @class
  * @extends HTMLElement
  * @classdesc Defines web component that displays a number that counts up when it is scrolled into view.
- * @param {HTMLSpanElement} animation The animation element.
- * @param {HTMLDivElement} container The container element.
- * @param {number} decimalPlaces The number of decimal places to display.
- * @param {number} duration The duration of the animation in milliseconds.
- * @param {boolean} isOnScreen Whether the element is on screen.
- * @param {function} observerCallback The callback method for the observer.
- * @param {number} animationStartTime The time the animation started.
- * @param {function} tween The method that advances the animation.
- * @param {number} [valueStart=0] The duration of the animation in milliseconds.
- * @param {number} [valueEnd=99] The starting number.
- * @param { { [key:string]: HTMLSpanElement} } refs
- * @attribute {string} [animation-duration=800] The duration of the animation in milliseconds.
- * @attribute {string} [animation-value-start=0] The starting number.
- * @attribute {string} [animation-value-end=99] The ending number.
- * @attribute {string} [suffix=""] The unit of measurement.
- * @attribute {string} [caption=""] The text to display below the number.
- *
  */
+
 class ScrollingStat extends HTMLElement {
+	// els = [""];
+	animationDuration = "0";
+	animationValueStart = "0";
+	animationValueEnd = "0";
+	colorBackground = "";
+	colorBorder = "";
+	colorPrimary = "";
+	colorSecondary = "";
+	contentSuffix = "";
+	contentCaption = "";
+	contentStylesheet = "";
 	refs;
-	animationStartTime;
 
-	/**
-	 * Attributes to observe for adding, removing, or changing.
-	 * @static
-	 * @returns {string[]} An array of attribute names to observe.
-	 */
+	// private variables
+	#isOnScreen = false;
+	#startTime = 0; // start time of the animation
+	#start = 0; // number to start the animation from
+	#end = 99; // number to end the animation at
+	#duration = Number(ScrollingStat.customAttributes["animation-duration"]); // duration of the animation
 
-	static get elementIds() {
-		return ["container", "animation", "suffix", "caption"];
-	}
-	static get observedAttributes() {
-		return [
-			"animation-duration",
-			"animation-value-start",
-			"animation-value-end",
-			"suffix",
-			"caption",
-		];
-	}
-	/**
-	 * Builds an object of refs elements from the elementIds array.
-	 * @param {*} context
-	 * @returns { { [key:string]: HTMLSpanElement} }
-	 */
-	static getDomElements(context) {
-		const result = /** @type { { [key:string]: HTMLSpanElement} } */ (
-			ScrollingStat.elementIds.reduce(
-				(acc, id) => ({
-					...acc,
-					[id]: /** @type {HTMLSpanElement} */ (
-						context.shadowRoot?.getElementById(id)
-					),
-				}),
-				{},
-			)
-		);
-		return result;
-	}
+	// TEMPLATE
+	// define elements here
+	static els = `
+			<style id="stylesheet" content></style>
+			<span id="container">
+				<span id="animation">0</span>
+				<span id="suffix" content></span>
+				<span id="caption" content>&nbsp;</span>
+			</span>`;
 
-	// get template
-	static get template() {
-		return `
-				<span id="container">
-					<span id="animation">0</span>
-					<span id="suffix"></span>
-					<span id="caption">&nbsp;</span>
-				</span>`;
-	}
-
-	// get styles
-	static get styles() {
-		return `
+	// define styles here
+	static styles = `
 			<style>
 				host:, * { margin:0; box-sizing:border-box ; }
 				#container {
 					align-items: baseline;
-					background: rgb(100%100%100%/5%);
+					background: var(--color-background, rgb(255 255 255 / 5%));
+					outline: 1px solid var(--color-border, transparent);
 					border-radius: .3em;
+					color: var(--color-primary, currentColor);
 					display: grid;
 					font-weight: 800;
 					gap: .3em;
 					grid: auto-flow/auto 1fr;
 					line-height: 1;
-					outline: 1px solid;
 					padding: 1.5em;
 					max-width: 12em;
 				}
@@ -104,6 +67,7 @@ class ScrollingStat extends HTMLElement {
 					font-size:1.9em;
 				}
 				#caption {
+					color: var(--color-secondary, currentColor);
 					font-size:.9em;
 					font-style:italic;
 					font-weight:400;
@@ -112,124 +76,336 @@ class ScrollingStat extends HTMLElement {
 					opacity:.9;
 				}
 		</style>`;
+
+	// define custom attributes here
+	/**
+	 * Returns an object. The keys are prop names and the values are default values.
+	 * @returns { { [key:string]: string} }
+	 */
+	static get customAttributes() {
+		// attribute, default
+		const values = [
+			["animation-duration", "800"],
+			["animation-value-end", "99"],
+			["animation-value-start", "0"],
+		];
+
+		// convert values to obj
+		const obj = values.reduce((acc, v) => ({ ...acc, [v[0]]: v[1] }), {});
+		return obj;
 	}
 
-	// getters and setters -- make attributes reflect in properties
-	get animationDuration() {
-		return this.getAttribute("animation-duration") || "800";
+	// TEMPLATE
+	// returns the template
+	static get template() {
+		// create a template
+		const template = document.createElement("template");
+		template.innerHTML = `
+			${this.styles}
+			${this.els}
+			`;
+		return template;
 	}
-	get animationDurationNumber() {
-		return Math.abs(parseInt(this.animationDuration));
+
+	// IDS
+	static get ids() {
+		return [...`${this.els + this.styles}`.matchAll(/id="([^"]+)"/g)].map(
+			(m) => m[1],
+		);
 	}
-	set animationDuration(value) {
-		this.setAttribute("animation-duration", value);
+
+	// REFS
+	/**
+	 * Builds an object of refs elements from the ids array.
+	 * @param {*} x = context
+	 * @returns { { [key:string]: HTMLSpanElement} }
+	 */
+	static getRefs(x) {
+		return /** @type { { [key:string]: HTMLSpanElement} } */ (
+			this.ids.reduce(
+				(acc, id) => ({
+					...acc,
+					[id]: /** @type {HTMLSpanElement} */ (
+						x.shadowRoot?.getElementById(id)
+					),
+				}),
+				{},
+			)
+		);
 	}
-	get animationValueStart() {
-		const attribute = this.getAttribute("animation-value-start");
-		if (attribute) {
-			return attribute;
-		} else {
-			const end = parseFloat(this.getAttribute("animationValueEnd") ?? "99");
-			return `${Math.abs(end) > 49 ? 0 : end > 0 ? 99 : -99}`;
+
+	// PROPS
+	static get getColorAttributes() {
+		const regex = /var\(--(.*?),(?: )*?(.*?)\)/g;
+		/** @type {{[key:string]: string}}j */
+		const obj = {};
+		let match;
+		while ((match = regex.exec(this.styles)) !== null) {
+			obj[match[1].trim()] = match[2].trim();
 		}
+		return obj;
 	}
-	get animationValueStartNumber() {
-		return parseFloat(this.animationValueStart);
+	/**
+	 * return an array of content props from elements with content attribute
+	 * @example
+	 * // returns ["content-suffix", "content-caption", "content-stylesheet"]
+	 * this.contentProps
+	 * */
+	static get #contentProps() {
+		const regex = /<.*?id="(.*?)"(.*?)content(.*?)>/g;
+		const matches = this.findMatches(regex, this.els);
+		return matches.map((v) => `content-${v}`);
 	}
-	set animationValueStart(value) {
-		this.setAttribute("animation-value-start", value);
-	}
-	get animationValueEnd() {
-		return this.getAttribute("animation-value-end") || "99";
-	}
-	get animationValueEndNumber() {
-		return parseFloat(this.animationValueEnd);
-	}
-	set animationValueEnd(value) {
-		this.setAttribute("animation-value-end", value);
-	}
-	get animationValueCurrentNumber() {
-		const start = this.animationValueStartNumber;
-		const end = this.animationValueEndNumber;
-		const progress = this.animationProgress;
-		return start + (end - start) * progress;
-	}
-	get animationValueCurrentString() {
-		return this.animationValueCurrentNumber.toFixed(this.decimalPlaces);
-	}
-	get suffix() {
-		return this.getAttribute("suffix") || "";
-	}
-	set suffix(value) {
-		this.setAttribute("suffix", value);
-	}
-	get caption() {
-		return this.getAttribute("caption") || "";
-	}
-	set caption(value) {
-		this.setAttribute("caption", value);
+	/**  return an array of color properties from stylesheet variables
+	 * @example
+	 * // returns ["color-background", "color-border", "color-primary", "color-secondary"]
+	 * this.colorProps
+	 *  */
+	static get #colorProps() {
+		return Object.keys(this.getColorAttributes);
 	}
 
-	// derived
-	get decimalPlaces() {
-		return Math.min(
-			(`${this.animationValueEnd}`.split(".")[1] || "").length,
-			2,
-		);
-	}
-	get animationProgress() {
-		return Math.min(
-			(Date.now() - this.animationStartTime) / this.animationDurationNumber,
-			1,
-		);
-	}
-	get animationTimeElapsed() {
-		return Date.now() - this.animationStartTime;
+	// custom props
+	/**
+	 * return an array of custom props from the customAttributes method
+	 * @example
+	 * // returns ["animation-duration", "animation-value-start", "animation-value-end"]
+	 * this.customProps
+	 * */
+	static get #customProps() {
+		return Object.keys(this.customAttributes);
 	}
 
-	// triggers
-	/** @param {boolean} value */
-	set isOnScreen(value) {
-		this.animationStartTime = 0;
-		if (value) this.tween();
+	// OBSERVED ATTRIBUTES
+	static get observedAttributes() {
+		return [...this.#customProps, ...this.#contentProps, ...this.#colorProps];
 	}
 
-	// helpers
-	/** @param {string | undefined | null} str */
-	updateAnimationDisplay(str) {
-		this.refs.animation.textContent = str
-			? str
-			: this.animationValueCurrentString;
+	// COLOR
+	/**
+	 * updates the css variables for all color attributes
+	 * @param {*} x = context
+	 * @param {string} attr
+	 * */
+	static setColor(x, attr) {
+		x.refs.container.style.setProperty(`--${attr}`, x.getAttribute(attr));
+	}
+	/**
+	 * updates the css variables for all color attributes
+	 * @param {*} x = context */
+	static setAllColors(x) {
+		this.observedAttributes.forEach((attr) => {
+			if (attr.includes("color")) {
+				this.setColor(x, attr);
+			}
+		});
 	}
 
-	// constructor
+	// TEXT
+	/**
+	 * updates the textContent of an element based on the matching attribute
+	 * @param {*} x = context
+	 * @param {*} attr
+	 */
+	static setText(x, attr) {
+		x.refs[attr.split("-")[1]].textContent = x.getAttribute(attr);
+	}
+	/**
+	 * updates the textContent of elements based on the content attributes
+	 * @param {*} context */
+	static setAllText(context) {
+		this.observedAttributes.forEach((attr) => {
+			if (attr.includes("content")) {
+				context.refs[attr.split("-")[1]].textContent =
+					context.getAttribute(attr);
+			}
+		});
+	}
+
+	// TWEEN
+	/**
+	 * Tween method to animate the displayed number.
+	 * @method
+	 * @param {*} x = context */
+	static tween(x) {
+		// if startTime is zero, record new the start time
+		if (!x.startTime) x.startTime = Date.now();
+
+		// calculate elapsed time
+		const elapsed = Date.now() - x.startTime;
+
+		// calculate progress
+		const progress = Math.min(elapsed / x.duration, 1);
+
+		// calculate the current value, "V"
+		const current = x.start + (x.end - x.start) * progress;
+
+		// advance animation to the current value
+		x.updateAnimationText(current.toFixed(x.places));
+
+		// if the current time elapsed is less than the duration
+		// and the end value is not reached, request another frame
+		// otherwise, update the display with the end value
+		elapsed < x.duration
+			? window.requestAnimationFrame(() => this.tween(x))
+			: x.updateAnimationText(`${x.end}`);
+	}
+
+	// STATIC UTILS
+
+	/**
+	 * Converts a kebab-case string to camelCase.
+	 * @param {string} str */
+	static kebabToCamelCase(str) {
+		return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+	}
+
+	// find matches utility
+	/**
+	 * Generic static method to find matches in a string.
+	 * @param {string} str
+	 * @param {RegExp} regex
+	 * @returns {string[]}
+	 *  */
+	static findMatches(regex, str) {
+		const variables = new Set();
+		let match;
+		while ((match = regex.exec(str)) !== null) {
+			variables.add(match[1].trim());
+		}
+		return Array.from(variables);
+	}
+
+	/**
+	 * Get the number of decimal places in a string representation of a number.
+	 * @param {string} str */
+	static places(str, limit = 2) {
+		return Math.min((str.split(".")[1] || "").length, limit);
+	}
+
+	// CONSTRUCTOR
 	constructor() {
 		super();
 
-		// set initial values
-		this.animationStartTime = 0;
-		this.isOnScreen = false;
+		// programattically create getters and setters for each observed attribute
+		// our getters and setters will use the camelCase version of the attribute name
+		for (let attr of ScrollingStat.observedAttributes) {
+			const attrCamel = ScrollingStat.kebabToCamelCase(attr);
+			Object.defineProperty(this, attrCamel, {
+				get: function () {
+					// coerce to string in case the attribute is a number
+					return this.getAttribute(attr).toString();
+				},
+				set: function (value) {
+					this.setAttribute(attr, value);
+				},
+			});
+		}
 
 		// create a shadow root
 		this.attachShadow({ mode: "open" });
 
 		// create a template
-		const template = document.createElement("template");
-		template.innerHTML = `
-		${ScrollingStat.styles}
-		${ScrollingStat.template}
-		`;
+		const template = ScrollingStat.template;
 
 		// append the template content to the shadow DOM
 		this.shadowRoot?.appendChild(template.content.cloneNode(true));
 
+		// define refs elements
+		this.refs = ScrollingStat.getRefs(this);
+
 		// binding the parent context to the methods
 		this.connectedCallback = this.connectedCallback.bind(this);
 		this.observerCallback = this.observerCallback.bind(this);
-		this.tween = this.tween.bind(this);
+	}
 
-		// get refs elements
-		this.refs = ScrollingStat.getDomElements(this);
+	// getter and setter for animation start time -- private variable
+	get startTime() {
+		return this.#startTime;
+	}
+	set startTime(value) {
+		this.#startTime = value;
+	}
+	resetStartTime() {
+		this.#startTime = 0;
+	}
+
+	// getter and setter for isOnScreen -- private variable
+	get isOnScreen() {
+		return this.#isOnScreen;
+	}
+	/** @param {boolean} value */
+	set isOnScreen(value) {
+		this.#isOnScreen = value;
+		// when the element is scrolled into view, reset the start time and start the tween
+		if (value) {
+			this.#startTime = 0;
+			ScrollingStat.tween(this);
+		}
+	}
+
+	// getter and setter and updater for end -- private variable
+	get end() {
+		return this.#end;
+	}
+	set end(value) {
+		this.#end = value;
+	}
+	updateEnd() {
+		this.end = parseFloat(this.animationValueEnd) ?? 99;
+	}
+
+	// getter and setter and updater for start -- private variable
+	get start() {
+		return this.#start;
+	}
+	set start(value) {
+		this.#start = value;
+	}
+	updateStart() {
+		const raw = this.animationValueStart;
+		const end = this.end ?? 99;
+		const fb = () => (Math.abs(end) > 49 ? 0 : end > 0 ? 99 : -99);
+		this.start = raw ? parseFloat(raw) ?? 0 : fb();
+	}
+
+	// getter and setter and updater for duration -- private variable
+	get duration() {
+		return this.#duration;
+	}
+	set duration(value) {
+		console.log("duration", value);
+		this.#duration = value;
+	}
+	updateDuration() {
+		const raw = parseInt(this.animationDuration);
+		const def = parseInt(ScrollingStat.customAttributes["animation-duration"]);
+		if (raw) this.duration = raw;
+		else this.duration = def ?? 600;
+	}
+
+	// updaters
+	updatePlaces() {
+		this.places = ScrollingStat.places(this.animationValueEnd);
+	}
+
+	// NON-STATIC METHODS
+	/**
+	 * Updates the textContent of the animation element.
+	 * @param {string} str */
+	updateAnimationText(str) {
+		this.refs.animation.textContent = str ?? "";
+	}
+	resetAnimation() {
+		this.updateEnd();
+		this.updatePlaces();
+		this.updateStart();
+		this.updateDuration();
+		this.resetStartTime();
+	}
+	resetContent() {
+		ScrollingStat.setAllText(this);
+		ScrollingStat.setAllColors(this);
 	}
 	/**
 	 * Method invoked when the custom element is first connected to the document's DOM. Defines DOM elements, adds css styling, and starts the observer.
@@ -237,24 +413,22 @@ class ScrollingStat extends HTMLElement {
 	 * @returns {void}
 	 * @summary Creates the shadow DOM, add styles, and starts the observer.
 	 */
+
+	attributeChangedCallback() {
+		this.resetAnimation();
+		this.resetContent();
+	}
+
 	connectedCallback() {
 		// reset values
-		this.animationStartTime = 0;
-		this.isOnScreen = false;
+		this.resetAnimation();
+		this.resetContent();
 
-		// set text content
-		this.updateAnimationDisplay(this.animationValueStart);
-		this.refs.caption.textContent = this.caption;
-		this.refs.suffix.textContent = this.suffix;
-
-		// create the observer
-		const observer = new IntersectionObserver(this.observerCallback, {
+		// create and start the observer
+		new IntersectionObserver(this.observerCallback, {
 			rootMargin: "0%",
 			threshold: 0.5,
-		});
-
-		// start the observer
-		observer.observe(this.refs.container);
+		}).observe(this.refs.container);
 	}
 	/**
 	 * @param {IntersectionObserverEntry[]} entries
@@ -264,32 +438,10 @@ class ScrollingStat extends HTMLElement {
 	observerCallback(entries) {
 		// test if the element is on screen
 		const isOnScreen = entries[0].isIntersecting;
-
 		// update variable to match the state of the element
 		if (isOnScreen != this.isOnScreen) {
 			this.isOnScreen = isOnScreen;
 		}
-	}
-	/**
-	 * Tween Method
-	 * @method
-	 * @returns {void}
-	 * @example
-	 * tween(0, 99)
-	 */
-	tween() {
-		// if zero, record new the start time
-		if (!this.animationStartTime) this.animationStartTime = Date.now();
-
-		// advance the displayed number to the current value
-		this.updateAnimationDisplay(this.animationValueCurrentString);
-
-		// if the current time elapsed is less than the duration
-		// and the end value is not reached, request another frame
-		// otherwise, update the display with the end value
-		this.animationTimeElapsed < this.animationDurationNumber
-			? window.requestAnimationFrame(() => this.tween())
-			: this.updateAnimationDisplay(this.animationValueEnd);
 	}
 }
 customElements.define("scrolling-stat", ScrollingStat);
