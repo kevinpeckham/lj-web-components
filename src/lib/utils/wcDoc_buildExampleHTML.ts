98f5ee94e07e78309d910dba950412b6eb4c30f9
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-01-30 10:36:03 -0500

    Refactor buildExampleWcHTML function to accept attributes as an object

--- src/lib/utils/wcDoc_buildExampleHTML.ts
+++ src/lib/utils/wcDoc_buildExampleHTML.ts
@@ -1,13 +1,38 @@
 /* global WcDocumentation */
-export function buildExampleWcHTML(documentation: WcDocumentation) {
-	const { slug, attributes } = documentation;
-	const attributesArray = attributes.map((att) => {
-		const name = att.name;
-		const value = att.example;
-		const attribute = `${name}="${value}"`;
-		return value ? `\n  ${attribute}` : "";
-	});
-	const attributesString = attributesArray.join("");
+interface Attribute {
+	[key: string]: string;
+	example: string;
+}
+
+const attributesData: { [key: string]: Attribute } = {
+	"border-color": {
+		default: "currentColor",
+		example: "#000000",
+		description: "The color of the border",
+	},
+	"background-color": {
+		default: "currentColor",
+		example: "#000000",
+		description: "The color of the border",
+	},
+};
+
+type AttributesData = typeof attributesData;
+
+export function buildExampleWcHTML(slug: string, attributes: AttributesData) {
+	if (!slug || typeof slug !== "string") return "";
+	if (!attributes) return "";
+
+	const keys = Object.keys(attributes) ?? [];
+
+	const attributesArray =
+		keys.map((att) => {
+			const name = att ?? "";
+			const value = attributes[att]?.example ?? "";
+			const attribute = `${name}="${value}"`;
+			return value ? `\n  ${attribute}` : "";
+		}) ?? [];
+	const attributesString = attributesArray?.join("") ?? "";
 	const example = `<${slug} ${attributesString}>\n</${slug}>`;
 	return example;
 }
