63149d755f6ab9e854a6935f7c55c758f0b3086a
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-03-12 20:32:25 -0400

    playground

---
+++ src/routes/playground/+layout.svelte
@@ -0,0 +1,12 @@
+<!--
+@component
+Here's some documentation for this component.
+-->
+
+<script lang="ts">
+</script>
+
+<template lang="pug">
+	slot
+
+	|</template>
---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,40 @@
+<!--
+@component
+Here's some documentation for this component.
+-->
+
+<script lang="ts">
+	// modules
+	// import SiteHeader from "/src/lib/web-components/site-header/v0.1.1.js";
+	// import ComponentUtils from "/src/lib/web-components/component-utils/v0.1.1.js";
+</script>
+
+<template lang="pug">
+	svelte:head
+	//- css
+	link(
+		as="style",
+		href="/e/custom/tca.min.css",
+		rel="preload")
+	link(
+		href="/e/custom/tca.min.css",
+		rel="stylesheet")
+
+	//- module preloaders
+	link(
+		href="e/wc/site-header@0.1.1.min.js",
+		rel="modulepreload")
+	link(
+		href="e/wc/component-utils@0.1.1.min.js",
+		rel="modulepreload")
+
+	//- modules
+	//- script(
+	//- 	async,
+	//- 	src="e/wc/component-utils@0.1.1.min.js",
+	//- 	type="module")
+	//- script(
+	//- 	async,
+	//- 	src="e/wc/site-header@0.1.1.min.js",
+	//- 	type="module")
+	|</template>
