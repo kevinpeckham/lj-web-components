8580048c7d63f8deca46cba7e79b1303a32548e0
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-03-31 08:37:08 -0400

    refactor <site-footer>

--- src/lib/web-components/site-footer/v0.1.1.js
+++ src/lib/web-components/site-footer/v0.1.1.js
@@ -2,26 +2,19 @@
 import { ComponentUtils } from "/e/wc/component-utils@0.1.1.min.js";
 
 // type definitions
-/** @typedef {{ alt?: string; url?: string; width?:string; height?: string; }} Image */
 /** @typedef {{ label: string; url: string; title?: string; rel?: string }} Link */
-/** @typedef {{ heading?: string; text?: string; button?: Link; links?: Link[] }} NavMenuFlyout */
-/** @typedef {{ label?: string; flyout?: NavMenuFlyout; link?: Link }} NavMenuItem */
-/** @typedef {{ heading?: string; items: NavMenuItem[]; }} NavMenuSection */
-/** @typedef {{ label?: string; sections?: NavMenuSection[]; link?: Link; }} NavMenu */
-/** @typedef {{ button?: Link; brand?: { link: Link; logo: Image; }; menus?: NavMenu[]; }} NavData */
-
 
 /** @copyright Lightning Jar 2024 - "Site Footer" web component */
 /** @author Kevin Peckham */
 /** @license MIT */
 /** @version 0.1.1 */
-/** {@link https://www.lj-cdn.dev/web-components/site-footer} */
+/** {@link https://cdn.lj.dev/web-components/site-footer} */
 
 /**
  * Copyright Statement Web Component
  * @name SiteFooter
  * @class
- * @published 2024-24-09
+ * @published 2024-03-31
  * @extends HTMLElement
  * @classdesc Defines web component that renders a simple responsive site footer with logo, links and copyright statement.
  * @attribute brand-image-alt | logo | The Competency Alliance logo | alt text of the brand image
@@ -29,7 +22,6 @@ import { ComponentUtils } from "/e/wc/component-utils@0.1.1.min.js";
  * @attribute brand-link-url | / | -- | url of the brand link
  * @attribute brand-link-title | website home | -- | title of the brand link
  * @attribute brand-width | 100px | 100px | width of the brand image
- *
  * @attribute button-link-label | Contact | Contact Us | text content of the copy button
  * @attribute button-link-url | -- | /contact | url of the copy button
  * @attribute button-link-title | -- | get in touch | title of the copy button
@@ -45,36 +37,12 @@ import { ComponentUtils } from "/e/wc/component-utils@0.1.1.min.js";
  * @attribute stylesheet | -- | -- | custom stylesheet content
  */
 class SiteFooter extends HTMLElement {
-	brandImageAlt = "";
-	brandImageUrl = "";
-	brandLinkTitle = "";
-	brandLinkUrl = "";
-	buttonLinkUrl = "";
-	buttonLinkLabel = "";
-	buttonLinkTitle = "";
-	colorAccent = "";
-	colorBackground = "";
-	colorPrimary = "";
-	colorShadow = "";
-	copyrightText = "";
-	dataJson = "";
-	dataJsonUrl = "";
-	fontFamily = "";
-	containerHeight = "";
-	legalLinksData = "";
-	menusData = "";
-	navLinksData = "";
-	stylesheet= "";
 
 // reference to class itself
 get c() { return SiteFooter };
 
-
 // ATTRIBUTES
-/**
- * Returns an object. The keys are prop names. The values are the default values for the props.
- * @returns { { [key:string]: string } }
- */
+/** @returns { { [key:string]: string } } */
 static get attributes() {
 	const values = {
 		"data-json": "",
@@ -100,67 +68,51 @@ static get attributes() {
 return values;
 }
 
-// OBSERVED ATTRIBUTES GETTER
-static get observedAttributes() { return Object.keys(this.attributes) }
-
-// ATTRIBUTE DEFAULT VALUE GETTER
-/** @param {string} attr */
-static getDefault(attr) { return this.attributes[attr] ?? "" }
-
-// PARSE DATA JSON - REMOTE DATA
-/** @returns {NavData} */
-get parsedData() {
-return this.dataJson ? JSON.parse(this.dataJson) : {};
+attValue(/** @type {string} att */ att) {
+	return this.getAttribute(att) ?? this.c.attributes[att] ?? "";
 }
 
-// PARSE DATA JSON - MENUS
-get parsedMenusData() {
-	return JSON.parse(this.menusData ?? "") ?? [];
-}
 
-// ELEMENTS MASTER LAYOUT GETTER
+// ELEMENTS
 get els() {
 	return `
-<style id="stylesheet">${this.stylesheet}</style>
+	<style id="stylesheet">${this.attValue('stylesheet')}}</style>
 <footer
 	id="container"
 	style="
-		--color-accent: ${this.colorAccent};
-		--color-background: ${this.colorBackground};
-		--color-primary: ${this.colorPrimary};
-		--color-shadow: ${this.colorShadow};
-		--container-height: ${this.containerHeight};
-		--font-family: ${this.fontFamily};">
-
-
-		<!-- top section: brand, links, button -->
+		--color-accent: ${this.attValue('color-accent')};
+		--color-background: ${this.attValue('color-background')};
+		--color-primary: ${this.attValue('color-primary')};
+		--color-shadow: ${this.attValue('color-shadow')};
+		--container-height: ${this.attValue('container-height')};
+		--font-family: ${this.attValue('font-family')};">
 		<div id="top-section">
 			<a
 				id="brand-link"
-				href="${this.brandLinkUrl}"
-				title="${this.brandLinkTitle}">
+				href="${this.attValue('brand-link-url')}"
+				title="${this.attValue('brand-link-title')}">
 				<img
 					id="brand-image"
-					alt="${this.brandImageAlt}"
-					src="${this.brandImageUrl}"
+					alt="${this.attValue('brand-image-alt')}"
+					src="${this.attValue('brand-image-url')}"
 					loading="lazy" />
 			</a>
 			<div id="nav-links">
-			${JSON.parse(this.navLinksData).map((
+			${JSON.parse(this.attValue('nav-links-data')).map((
 				/** @type {Link} link*/
 				link) => `<a class="nav-link" href="${link.url}">${link.label}</a>`).join("")}
 			</div>
 			<a
 				id="button-link"
-				href="${this.buttonLinkUrl}">${this.buttonLinkLabel}
+				href="${this.attValue('button-link-url')}">${this.attValue('button-link-label')}
 			</a>
 		</div>
 
 		<!-- bottom section: legal and attribution -->
 		<div id="bottom-section">
-			<div id="copyright-statement">${this.copyrightText}</div>
+			<div id="copyright-statement">${this.attValue('copyright-text')}</div>
 			<div id="legal-links">
-			${JSON.parse(this.legalLinksData).map((
+			${JSON.parse(this.attValue('legal-links-data')).map((
 				/** @type {Link} link*/
 				link) => `<a href="${link.url}" rel="noopener noreferrer">${link.label}</a>`).join("")}
 
@@ -170,24 +122,15 @@ get els() {
 </footer>`.trim();
 }
 
-//${JSON.parse(this.legalLinksData).map((link) => 'test')};
-//<!-- `<a href="${link.url}" rel="noopener noreferrer">${link.label}</a>`).join("") -->
-
-get preflight() {
-	return `*,::before,::after {box-sizing:border-box;border-width:0;border-style:solid;border-color:currentColor}::before,::after {--tw-content:""}html,:host {line-height:1.5;-webkit-text-size-adjust:100%;-moz-tab-size:4;tab-size:4;font-family:theme( "fontFamily.sans", ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" );font-feature-settings:normal;font-variation-settings:normal;-webkit-tap-highlight-color:transparent}body {margin:0;line-height:inherit}hr {height:0;color:inherit;border-top-width:1px}abbr:where([title]) {text-decoration:underline dotted}h1,h2,h3,h4,h5,h6 {font-size:inherit;font-weight:inherit}a {color:inherit;text-decoration:inherit}b,strong {font-weight:bolder}code,kbd,samp,pre {font-family:theme( "fontFamily.mono", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace );font-feature-settings:normal;font-variation-settings:normal;font-size:1em}small {font-size:80%}sub,sup {font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub {bottom:-0.25em}sup {top:-0.5em}table {text-indent:0;border-color:inherit;border-collapse:collapse}button,input,optgroup,select,textarea {font-family:inherit;font-feature-settings:inherit;font-variation-settings:inherit;font-size:100%;font-weight:inherit;line-height:inherit;color:inherit;margin:0;padding:0}button,select {text-transform:none}button,[type="button"],[type="reset"],[type="submit"] {-webkit-appearance:button;background-color:transparent;background-image:none}:-moz-focusring {outline:auto}:-moz-ui-invalid {box-shadow:none}progress {vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button {height:auto}[type="search"] {-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration {-webkit-appearance:none}::-webkit-file-upload-button {-webkit-appearance:button;font:inherit}summary {display:list-item}blockquote,dl,dd,h1,h2,h3,h4,h5,h6,hr,figure,p,pre {margin:0}fieldset {margin:0;padding:0}legend {padding:0}ol,ul,menu {list-style:none;margin:0;padding:0}dialog {padding:0}textarea {resize:vertical}input::placeholder,textarea::placeholder {opacity:1;color:theme("colors.gray.400", #9ca3af)}button,[role="button"] {cursor:pointer}:disabled {cursor:default}img,svg,video,canvas,audio,iframe,embed,object {display:block;vertical-align:middle}img,video {max-width:100%;height:auto}[hidden] {display:none}`
-}
-
 get styles() {
 	return `
-<style id="preflight">${this.preflight}</style>
+<style id="preflight">${ComponentUtils.preflight}</style>
 <style id="base">
 	:host {
 		box-sizing: border-box;
 		border-width: 0;
 		border-style: solid;
 	}
-
-	/* container */
 	#container {
 		align-items: start;
 		color: var(--color-primary, #D8F9FF);
@@ -214,8 +157,6 @@ get styles() {
 			padding:56px 80px;
 		}
 	}
-
-/* top section */
 #top-section {
 	align-items: center;
 	display: block;
@@ -228,7 +169,6 @@ get styles() {
 		display:flex;
 	}
 }
-
 #brand-link {
 	display: block;
 	height: 100%;
@@ -270,9 +210,6 @@ get styles() {
 		margin-bottom:0;
 	}
 }
-
-
-/* button */
 #button-link {
 	border-radius: 0.35em;
 	border: solid 0.1em var(--color-primary, #fff);
@@ -287,7 +224,6 @@ get styles() {
 	white-space: nowrap;
 	text-decoration: none;
 }
-/* button - lg */
 @media (min-width: 1024px) {
 	#button-link {
 		border-color: color-mix(
@@ -322,8 +258,6 @@ get styles() {
 		}
 	}
 }
-
-/* bottom section */
 #bottom-section {
 	align-items: center;
 	column-gap: 1rem;
@@ -352,10 +286,7 @@ get styles() {
 #bottom-section #legal-links a:hover {
 	opacity: 1;
 	text-decoration-color: currentColor;
-
 }
-
-
 </style>`.trim();
 }
 
@@ -366,31 +297,15 @@ get template() {
 	return template;
 }
 
-// IDS
-get ids() {
- const ids = [...`${this.styles}${this.els}`.matchAll(/id="([^"]+)"/g)].map((m) => m[1]);
- return ids;
-}
-
 // CONSTRUCTOR
 constructor() {
 	super();
-	// programattically create getters and setters for each observed attribute
-	ComponentUtils.createOAGS(this.c, this);
-
-	// create a shadow root
 	this.attachShadow({ mode: "open" });
-
 }
 
 // CONNECTED CALLBACK
 connectedCallback() {
-		// append the template content to the shadow DOM
 		this.shadowRoot?.appendChild(this.template.content.cloneNode(true))
-
-		// define refs elements
-		this.refs = ComponentUtils.getRefs(this.c, this);
-
 }
 
 
